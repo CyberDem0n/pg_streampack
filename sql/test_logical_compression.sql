78f05a2fd04cf5af5d32807d348996fb3a2606bf
Alexander Kukushkin <cyberdemn@gmail.com>
2025-09-09 07:57:52 +0200

    Decompress only in streaming mode
    In addition to that make regression tests less flaky

--- sql/test_logical_compression.sql
+++ sql/test_logical_compression.sql
@@ -10,11 +10,20 @@ CREATE TABLE test(id int not null primary key, name text);
 CREATE SUBSCRIPTION test CONNECTION 'dbname=src' PUBLICATION test WITH (CONNECT = FALSE);
 ALTER SUBSCRIPTION test ENABLE;
 ALTER SUBSCRIPTION test REFRESH PUBLICATION;
-CREATE FUNCTION wait_subscription()
+CREATE FUNCTION wait_subscription(len bigint)
  RETURNS void AS $function$
 DECLARE
     loop_cnt int DEFAULT 0;
 BEGIN
+	WHILE (SELECT SUM(length(name)) FROM test) != len LOOP
+		SELECT SUM(length(name)) FROM test;
+        loop_cnt := loop_cnt + 1;
+        IF loop_cnt > 600 THEN
+            RAISE EXCEPTION 'Subscription did not caught up after 1 minute';
+        ELSE
+            PERFORM pg_sleep(0.1);
+        END IF;
+    END LOOP;
     LOOP
         PERFORM 1 FROM pg_stat_subscription
         WHERE pg_current_wal_lsn() - received_lsn = 0
@@ -29,13 +38,11 @@ BEGIN
     END LOOP;
 END
 $function$ LANGUAGE plpgsql;
-SELECT wait_subscription();
-SELECT SUM(length(name)) FROM test;
+SELECT wait_subscription(10155);
 \c src
 UPDATE test SET name = name || name;
 \c dst
-SELECT wait_subscription();
-SELECT SUM(length(name)) FROM test;
+SELECT wait_subscription(20310);
 DROP SUBSCRIPTION test;
 \c postgres
 DROP DATABASE dst;
